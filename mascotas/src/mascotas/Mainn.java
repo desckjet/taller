f09f1e25bb8a343207370190b314fe89df646f0d
desckjet <31544772+desckjet@users.noreply.github.com>
2018-09-01 22:20:41 -0500

    se agregaron los metodos del cliente

--- mascotas/src/mascotas/Mainn.java
+++ mascotas/src/mascotas/Mainn.java
@@ -1,11 +1,11 @@
 package mascotas;
 
+import exceptions.NoExisteException;
 import mascotas.ui.MenuPrincipal;
 
 public class Mainn {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NoExisteException {
 		
 		centralCliente cliente = new centralCliente();
 		centralMascota mascota = new centralMascota();
@@ -24,11 +24,24 @@ public class Mainn {
 		cliente.insertarFinal(new Cliente(4, "nulll", "cra 78 # 62-05", 5412035,null));
 		cliente.insertarFinal(new Cliente(5, "chon", "cll 20 # 120-327", 7412035,null));
 		
+		/*cliente.insertarInicio(new Cliente(1, "negro", "cra 28 # 45-26", 4523105, mascota));
+		cliente.insertarInicio(new Cliente(2, "juicio", "cll 58 # 12-32", 4103652,mascota2));
+		cliente.insertarInicio(new Cliente(3, "pedro", "cra 56 # 02-75", 9632051,null));
+		cliente.insertarInicio(new Cliente(4, "nulll", "cra 78 # 62-05", 5412035,null));
+		cliente.insertarInicio(new Cliente(5, "chon", "cll 20 # 120-327", 7412035,null));*/
+		
+		//cliente.insertarAntesDe(1, new Cliente(3, "pedro", "cra 56 # 02-75", 9632051,null));
+		//cliente.insertarDespuesDe(3, new Cliente(3, "pedro", "cra 56 # 02-75", 9632051,null));
+		
+		cliente.eliminar(4);
+		
+		// lista todos los clientes
 		cliente.listar();
 		
-		//mascota.verMascotas(1);
-		cliente.verMascotas(2);
 		//System.out.println(mascota.total());
+		//recibe el id del cliente y muestra sus mascotas
+		//cliente.verMascotas(2);
+		
 		
 		//System.out.println(cliente.buscarUltimo().getNombre());
 		new MenuPrincipal().setVisible(true);
